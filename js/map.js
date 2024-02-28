a952d4344c7c00608039d75b99a84e06164928a1
Saigesp <saigesp@gmail.com>
2024-02-28 17:25:12 +0100

    update data

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ d3.csv("data/encuentros.csv", (data) => {
     const dates = data.Fecha.split('/');
     const times = data.Hora.split(':');
     const date = new Date(dates[2], dates[1] - 1, dates[0], times[0], times[1]);
-    const icon = date > new Date() ? iconOk : iconPass;
+    const icon = date > new Date() && data.Link ? iconOk : iconPass;
     const link = date > new Date() && data.Link ? data.Link : "https://asambleasumar.es/es/";
     const marker = L.marker([data.Latitud, data.Longitud], { icon });
     marker.addTo(map);
