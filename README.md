a55707cc2a1f3d8d9e8701d99af0561173060f46
Saigesp <saigesp@gmail.com>
2024-02-28 16:14:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mapa Encuentros I Asamblea Sumar
---
+++ js/map.js
@@ -0,0 +1,26 @@
+
+const map = L.map('map').setView([39.8376017, -4.3978819], 6);
+L.tileLayer.provider('CartoDB.Voyager').addTo(map);
+
+const markers = [];
+
+let popup = null;
+
+const iconOk = L.icon({
+    iconUrl: '/img/marker-icon-magenta.png', iconAnchor: [12.5, 41]
+});
+const iconPass = L.icon({
+    iconUrl: '/img/marker-icon-grey.png', iconAnchor: [12.5, 41]
+});
+
+d3.csv("data/encuentros.csv", (data) => {
+    const dates = data.Fecha.split('/');
+    const times = data.Hora.split(':');
+    const date = new Date(dates[2], dates[1] - 1, dates[0], times[0], times[1]);
+    const icon = date > new Date() ? iconOk : iconPass;
+    const link = date > new Date() && data.Link ? data.Link : "https://asambleasumar.es/es/";
+    const marker = L.marker([data.Latitud, data.Longitud], { icon });
+    marker.addTo(map);
+    marker.bindPopup(`<a href="${link}" target="_blank" rel="noopener noreferrer">${data['Título']}</a><br>${data.Fecha} - ${data.Hora}`);
+    markers.push(marker);
+});
